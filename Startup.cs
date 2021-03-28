6574e0f0d674fec97e1565817faf6a158d8f05ab
Seth Cleaver <seth.c@kast.io>
2021-03-28 13:47:08 +1000

    Upgrade to OC 1.0.0-rc2-16024

--- Startup.cs
+++ Startup.cs
@@ -1,11 +1,9 @@
 using Lombiq.VueJs.Services;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using OrchardCore.DisplayManagement.Descriptors.ShapeTemplateStrategy;
-using OrchardCore.Modules;
 using OrchardCore.ResourceManagement;
-using System;
+using Microsoft.Extensions.Options;
+using OrchardCore.Modules;
 
 namespace Lombiq.VueJs
 {
@@ -13,13 +11,9 @@ namespace Lombiq.VueJs
     {
         public override void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IShapeTemplateHarvester, VueComponentTemplateHarvester>();
-            services.AddScoped<IResourceManifestProvider, ResourceManifest>();
-        }
+            services.AddTransient<IConfigureOptions<ResourceManagementOptions>, ResourceManagementOptionsConfiguration>();
 
-        public override void Configure(IApplicationBuilder app, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
-        {
-            // No need for anything here yet.
+            services.AddScoped<IShapeTemplateHarvester, VueComponentTemplateHarvester>();
         }
     }
 }
